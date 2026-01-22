447218017dcc183d9e880ab16431de162ba85f3f
Anna Balesko <babasheldona@icloud.com>
2026-01-22 15:32:59 +0100

    add structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,60 @@
-import css from './App.module.css'
+import toast, { Toaster } from "react-hot-toast";
+import SearchBar from "../SearchBar/SearchBar";
+import css from "./App.module.css";
+import { fetchMovie } from "../../services/movieService";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import { useState } from "react";
+import type { Movie } from "../../types/movie";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 
 export default function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedMovie(null);
+  };
+
+  const handleSelectMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true);
+  };
+
+  const handleSearch = async (query: string) => {
+    try {
+      setMovies([]);
+      setIsLoading(true);
+      setIsError(false);
+      const data = await fetchMovie(query);
+      if (data.length === 0) {
+        toast.error("No movies found for your request.");
+        return;
+      }
+      setMovies(data);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <div className={css.app}></div>
-  )
+    <div className={css.app}>
+      {isModalOpen && selectedMovie && (
+        <MovieModal onClose={closeModal} movie={selectedMovie} />
+      )}
+      <SearchBar onSubmit={handleSearch} />
+      {movies.length > 0 && (
+        <MovieGrid onSelect={handleSelectMovie} movies={movies} />
+      )}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      <Toaster />
+    </div>
+  );
 }
