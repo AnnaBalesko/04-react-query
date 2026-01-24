a60ee011c2a71e5675c14275018f6c45bf396162
Anna Balesko <babasheldona@icloud.com>
2026-01-24 23:08:52 +0100

    add pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,23 @@
 import toast, { Toaster } from "react-hot-toast";
 import SearchBar from "../SearchBar/SearchBar";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import css from "./App.module.css";
-import {fetchMovies } from "../../services/movieService";
+import { fetchMovies } from "../../services/movieService";
+import type { MoviesRespons } from "../../services/movieService";
 import MovieGrid from "../MovieGrid/MovieGrid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Movie } from "../../types/movie";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
+import ReactPaginate from "react-paginate";
 
 export default function App() {
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [movies, setMovies] = useState<Movie[]>([]);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-  
+
   const closeModal = () => {
     setIsModalOpen(false);
     setSelectedMovie(null);
@@ -27,33 +29,48 @@ export default function App() {
   };
 
   const handleSearch = async (query: string) => {
-    try {
-      setMovies([]);
-      setIsLoading(true);
-      setIsError(false);
-      const data = await fetchMovies(query);
-      if (data.length === 0) {
-        toast.error("No movies found for your request.");
-        return;
-      }
-      setMovies(data);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+    setQuery(query);
+    setPage(1);
   };
+
+  const { data, isLoading, isError } = useQuery<MoviesRespons>({
+    queryKey: ["movies", query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: Boolean(query),
+    placeholderData: keepPreviousData,
+  });
+  const movies = data?.results ?? [];
+  const totalPages = data?.total_pages ?? 0;
+
+  useEffect(() => {
+    if (query && data && data.results.length === 0) {
+      toast.error("No movies found for your request.");
+    }
+  }, [data, query]);
+
   return (
     <div className={css.app}>
       {isModalOpen && selectedMovie && (
         <MovieModal onClose={closeModal} movie={selectedMovie} />
       )}
       <SearchBar onSubmit={handleSearch} />
-      {movies.length > 0 && (
-        <MovieGrid onSelect={handleSelectMovie} movies={movies} />
+      {totalPages > 1 && (
+        <ReactPaginate
+          pageCount={totalPages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setPage(selected + 1)}
+          forcePage={page - 1}
+          containerClassName={css.pagination}
+          activeClassName={css.active}
+          nextLabel="→"
+          previousLabel="←"
+        />
       )}
+      {data && <MovieGrid onSelect={handleSelectMovie} movies={movies} />}
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
+      
       <Toaster />
     </div>
   );
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
-interface MoviesRespons {
+export interface MoviesRespons {
   results: Movie[];
+  total_pages: number;
 }
 
-export const fetchMovies = async (query: string): Promise<Movie[]> => {
+export const fetchMovies = async (query: string, page:number): Promise<MoviesRespons> => {
   const config = {
     params: {
       query,
+      page,
     },
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
@@ -18,5 +20,5 @@ export const fetchMovies = async (query: string): Promise<Movie[]> => {
     "https://api.themoviedb.org/3/search/movie",
     config,
   );
-  return response.data.results;
+  return response.data;
 };
